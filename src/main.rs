a955cd2a92cee5ec3cacc88eb03a0b0e38ca7ace
KoChan- <needahentai@gmail.com>
2020-02-14 18:14:04 +0300

    Великое обновление

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use rand::Rng;
 
 fn main() {
-    let mut matrix = vec![];
-    let height = 10;
-    let width = 10;
-
-    for _ in 0..width {
-        let mut mx = vec![];
-        for _ in 0..height {
-            mx.push(rand::thread_rng().gen_range(0, 9));
-        }
-        matrix.push(mx);
-    }
-
-    for i in matrix.iter() {
-        println!("{:?}", i);
-    }
+    print!("H");
+    print!("e");
+    print!("l");
+    print!("l");
+    print!("o");
+    print!(" ");
+    print!("w");
+    print!("o");
+    print!("r");
+    print!("l");
+    print!("d");
+    print!("!");
+    println!("");
 }
