6b5b039434e50be427d8bc03402160c079c775b5
KoChan- <needahentai@gmail.com>
2020-02-14 18:19:39 +0300

    Великое обновление

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,22 @@
+
+#[allow(dead_code)]
 mod utils {
-    pub fn stss(s: String) {}
+    pub fn string_to_str() {}
 }
 
-pub fn string_to_static_str(s: String) -> &'static str {
+#[allow(dead_code)]
+pub fn string_to_str(s: String) -> &'static str {
     Box::leak(s.into_boxed_str())
+}
+
+#[macro_export]
+macro_rules! anime {
+    ( $( $n :expr ),* ) => {
+        {
+            $(
+                print!("{} ", $n);
+            )*
+            println!("");
+        }
+    }
 }
